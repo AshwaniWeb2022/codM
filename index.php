1b4f3ca069a3864856152d3ef408779b6957c162
hardik <hardiksharma7919@gmail.com>
2024-01-19 19:36:16 +0530

    added pseudo elements

--- index.php
+++ index.php
@@ -23,43 +23,122 @@
     </div>
 </section>
 <section class="container-fluid">
-  <div class="row">
-    <div class="col-lg-6 bg-light ">
-      <div class="float-lg-left pt-5">      
-          <h1><span class="text-decoration:bold">Our certificate </h1>
-            <p>several selected clients who already believe in our service </p>
-      </div>
+    <div class="row">
+        <div class="col-lg-6 bg-light ">
+            <div class="float-lg-left pt-5">
+                <h1><span class="text-decoration:bold">Our certificate </span> </h1>
+                <p>several selected clients who already believe in our service </p>
+            </div>
+        </div>
+        <div class="col-lg-6 pe-0 bg-light">
+            <div class="client owl-carousel  text-center" id="ourclient">
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/omniStudioDeveloper.webp"
+                        alt="omniStudioDeveloper" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/omniStudioConsultant.webp"
+                        alt="omniStudioConsultant" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/platformAppBuilder.webp"
+                        alt="platformAppBuilder" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/PD1.webp" alt="PD1" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/PD2.webp" alt="PD2" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/CPQSpecialist.webp" alt="CPQSpecialist"
+                        style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="https://codmsoftware.com/assect/upload/footer/B2CSolutionArchitect.webp"
+                        alt="B2CSolutionArchitect" style="width:100px;">
+                </div>
+                <div class="item">
+                    <img src="	https://codmsoftware.com/assect/upload/footer/applicationArchitect.webp"
+                        alt="applicationArchitect" style="width:100px;">
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-6 pe-0 bg-light">
-    <div class="client owl-carousel  text-center" id="ourclient">
-          <div class="item">
-          <img src="https://codmsoftware.com/assect/upload/footer/omniStudioDeveloper.webp" alt="omniStudioDeveloper" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/omniStudioConsultant.webp" alt="omniStudioConsultant" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/platformAppBuilder.webp" alt="platformAppBuilder" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/PD1.webp" alt="PD1" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/PD2.webp"alt="PD2" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/CPQSpecialist.webp" alt="CPQSpecialist" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="https://codmsoftware.com/assect/upload/footer/B2CSolutionArchitect.webp" alt="B2CSolutionArchitect" style="width:100px;">
-          </div>
-          <div class="item">
-            <img src="	https://codmsoftware.com/assect/upload/footer/applicationArchitect.webp" alt="applicationArchitect" style="width:100px;">
-          </div>
-    </div>   
-  </div>     
-  </div>    
 
 </section>
-  
-    <?php include 'app/footer.php';?>
+
+<section class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="sudoBGColor">
+                <div class="sudo"></div>
+                <h1>How can we help your businesses ?<h1>
+                        <p>we build ready made website, mobile applications, and elaborate online
+                            businesses services.
+                        <p>
+                        <div class="dotWrapper">
+                            <img src="assect/img/DotOrnament.png" alt="DotImage">
+                        </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 pe-0">
+            <div class="sudoBGColor2">
+                <div class="sudo2"></div>
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="card" style="width:18rem; g-4">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body" style="width:18rem;"">
+        <h5 class=" card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    <div class="float-lg-left">
+                        <div class="card" style="width:18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body" style="width:18rem;">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 pe-0">
+                        <div class="row row-cols-1 row-cols-md-2 g-4">
+                            <div class="card" style="width:18rem;">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body" style="width:18rem;">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a
+                                        natural
+                                        lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="card" style="width:18rem;">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body" style="width:18rem;"">
+                                     <h5 class=" card-title">Card title</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a
+                                            natural
+                                            lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</section>
+<section class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6">
+      <img src="C:\xampp\htdocs\CodM\assect\img\unsplash_bzqU01v-G54.png">
+    </div>
+  </div>
+</section>      
+<?php include 'app/footer.php';?>
