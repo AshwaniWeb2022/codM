7f43506c19672f39e61f074a46c2296f7bc9a4fa
hardik <hardiksharma7919@gmail.com>
2024-01-19 12:20:41 +0530

    carousel updated

--- index.php
+++ index.php
@@ -1,30 +1,65 @@
 <?php include 'app/header.php';?>
 <section class="container-fluid">
-<div class="row">
-    <div class="col-lg-6">
-        <div class="headerMainContainerLeft pt-5">
-            <div class="headerMain-wapper">
-            <h1>Optimizing Sales with Salesforce CRM Solutions</h1>
-            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum, veritatis dignissimos fugit recusandae tenetur.</p>
-            <a href="#" class="btnContact text-decoration-none text-white">Contact Now</a>
-            <div class="dotWrapper">
-                <img src="assect/img/DotOrnament.png" alt="DotImage">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="headerMainContainerLeft pt-5">
+                <div class="headerMain-wapper">
+                    <h1>Optimizing Sales with Salesforce CRM Solutions</h1>
+                    <p class="mb-5">Salesforce is a popular CRM tool for support, sales, and marketing teams worldwide.
+                        Salesforce services allow businesses to use cloud technology to better connect with partners,
+                        customers, and potential customers</p>
+                    <a href="#" class="btnContact text-decoration-none text-white">Contact Now</a>
+                    <div class="dotWrapper">
+                        <img src="assect/img/DotOrnament.png" alt="DotImage">
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="col-lg-6 pe-0">
+            <div class="headerMainContainerright">
+                <img src="assect/img/home_banner.jpg" alt="banner" class="w-100">
             </div>
         </div>
     </div>
-    <div class="col-lg-6 pe-0">
-        <div class="headerMainContainerright">
-        <img src="assect/img/home_banner.jpg" alt="banner" class="w-100">
-        </div>
-    </div>
-</div>
 </section>
 <section class="container-fluid">
-<div class="row">
-    <div class="col-lg-4"></div>
-    <div class="col-lg-8"></div>
-</div>
-</section>
+  <div class="row">
+    <div class="col-lg-6 bg-light ">
+      <div class="float-lg-left pt-5">      
+          <h1><span class="text-decoration:bold">Our certificate </h1>
+            <p>several selected clients who already believe in our service </p>
+      </div>
+    </div>
+    <div class="col-lg-6 pe-0 bg-light">
+    <div class="client owl-carousel  text-center" id="ourclient">
+          <div class="item">
+          <img src="https://codmsoftware.com/assect/upload/footer/omniStudioDeveloper.webp" alt="omniStudioDeveloper" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/omniStudioConsultant.webp" alt="omniStudioConsultant" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/platformAppBuilder.webp" alt="platformAppBuilder" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/PD1.webp" alt="PD1" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/PD2.webp"alt="PD2" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/CPQSpecialist.webp" alt="CPQSpecialist" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="https://codmsoftware.com/assect/upload/footer/B2CSolutionArchitect.webp" alt="B2CSolutionArchitect" style="width:100px;">
+          </div>
+          <div class="item">
+            <img src="	https://codmsoftware.com/assect/upload/footer/applicationArchitect.webp" alt="applicationArchitect" style="width:100px;">
+          </div>
+    </div>   
+  </div>     
+  </div>    
 
-<?php include 'app/footer.php';?>
+</section>
+  
+    <?php include 'app/footer.php';?>
