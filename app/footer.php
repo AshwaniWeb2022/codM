d9d101ddb5f4d05931e3b5f9dda51e7bbacbf346
hardik <hardiksharma7919@gmail.com>
2024-01-19 12:28:19 +0530

    carousel

--- app/footer.php
+++ app/footer.php
@@ -1,4 +1,41 @@
 <!-- bootstrap js bundle  -->
 <script src="assect/js/bootstrap.bundle.min.js"></script>
+
+</- - carousel - - >
+<!-- jQuary start -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- client section  -->
+<script>
+  $('#ourclient').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    slideSpeed: 1500,
+    responsive: {
+      0: {
+        items: 4
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 6
+      },
+      1400: {
+        items: 6
+      }
+    }
+  })
+</script>
+<!-- jQuary End -->
+
+<!-- jQuary  magnific-popup js  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js" integrity="sha512-IsNh5E3eYy3tr/JiX2Yx4vsCujtkhwl7SLqgnwLNgf04Hrt9BT9SXlLlZlWx+OK4ndzAoALhsMNcCmkggjZB1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
 </body>
 </html>
