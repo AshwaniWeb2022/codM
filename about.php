5cad2c3c6100ba225626c2fdf3345f8c7c86658f
hardik <hardiksharma7919@gmail.com>
2024-01-22 20:12:44 +0530

    About page

---
+++ about.php
@@ -0,0 +1,126 @@
+ <!-- For including header section  -->
+ <?php include 'app/header.php';?>
+ <section class="container-fluid">
+     <div class="row">
+         <div class="col-lg-6">
+             <div class="aboutPageHeadleft pt-5">
+                 <div class="aboutPageHead">
+
+                     <div class="Partner-Image">
+                         <img src="assect\img\salesforce partner 1.png" alt="PartnerImage" class="center">
+                     </div>
+                     <div class="star-Image">
+                         <img src="assect\img\stars.png" alt="stars-Image" class="w-50">
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-lg-6 pe-0">
+             <div class="aboutPageHeadright">
+                 <img src="assect\img\Rectangle 57.png" alt="banner" class="w-100">
+             </div>
+         </div>
+     </div>
+ </section>
+ <section class="container-fluid">
+     <div class="row">
+         <div class="col-lg-6 bg-light ">
+             <div class="float-lg-left pt-5">
+                 <h1 class="fs-4 fw-bold text-center">Our certificate</h1>
+                 <p>several selected clients who already believe in our service </p>
+             </div>
+         </div>
+         <div class="col-lg-6 pe-0 bg-light">
+             <div class="client owl-carousel  text-center" id="ourclient">
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/omniStudioDeveloper.webp"
+                         alt="omniStudioDeveloper" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/omniStudioConsultant.webp"
+                         alt="omniStudioConsultant" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/platformAppBuilder.webp"
+                         alt="platformAppBuilder" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/PD1.webp" alt="PD1" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/PD2.webp" alt="PD2" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/CPQSpecialist.webp" alt="CPQSpecialist"
+                         style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="https://codmsoftware.com/assect/upload/footer/B2CSolutionArchitect.webp"
+                         alt="B2CSolutionArchitect" style="width:100px;">
+                 </div>
+                 <div class="item">
+                     <img src="	https://codmsoftware.com/assect/upload/footer/applicationArchitect.webp"
+                         alt="applicationArchitect" style="width:100px;">
+                 </div>
+             </div>
+         </div>
+     </div>
+
+ </section>
+ <section class="container-fluid">
+     <div class="row">
+         <div class="col-lg-6">
+             <div class="aboutPagemiddleleft pt-5">
+                 <div class="aboutPagemiddleleft">
+
+                     <div class="rectangle">
+                         <img src="assect\img\Rectangle 60.png" alt="banner" class="w-100">
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-lg-6 pe-0">
+             <div class="mt-5 Text-between"></div>
+             <h2 class="fs-1 fw-bold text-center me-5">Grow your businesses with our CRM <br>Tools & Project
+                 Management</br>
+             </h2>
+             <h3 class="mt-5 fs-4 fw-bolder">About CodM software</h3>
+             <p class="mt-10 fs-4 fw-normal">Looking for certified and experienced developers to work on any project,you
+                 are in the right place
+             </p>
+             <img src="assect\img\Ellipse 113.png" alt="target" style="height:200px; width:150px;" class="mt-5 pt-5 me-5">
+             <img src="assect\img\Ellipse 114 (1).png" alt="target" style="height:200px; width:150px;" class="mt-5 pt-5 me-5">
+             <img src="assect\img\Ellipse 115.png" alt="target" style="height:200px; width:150px;" class="mt-5 pt-5">
+
+         </div>
+
+     </div>
+ </section>
+ <section class="mt-5">
+     <div class="container-fluid">
+        <img src="assect\img\Rectangle 43.png" alt="banner" style="height:150px width:200px;" class="ms-5 pb-5 mb-5 me-5">
+         <img src="assect\img\Ellipse 100.png" alt="banner" style="height:150px width:200px;" class="ms-5 ps-5 me-5">
+         <img src="assect\img\logo-salesforce.png" alt="banner" style="height:150px width:200px;"class="ms-5 mb-5 pb-5">
+         
+         <h3 class="fs-2 fw-bold text-center">We offer customized solutions for various industries and ensure
+             smooth.</h3>
+        
+             <img src="assect\img\Rectangle 46.png" alt="banner" style="width:200px;" class="mb-5 pb-5">   
+            <img src="assect\img\Rectangle 44.png" alt="banner" style="width:200px;" class="mt-3 pt-3 me-5">
+            <img src="assect\img\Rectangle 48.png" alt="banner" style="width:200px;"class="ms-5 mt-5 pt-5">
+            <img src="assect\img\Ellipse 101.png" alt="banner" style="width:200px;"class="ms-5 mt-3 pt-3">
+            <img src="assect\img\Ellipse 103.png" alt="banner" style="width:200px;"class="ms-5 mt-2 pt-2">
+            <img src="assect\img\Rectangle 47.png" alt="banner" style="width:200px;"class="ms-5 mb-5 pb-5">
+         
+    </div>
+
+
+
+
+
+
+
+
+
+     <!-- /* For including footer section */ -->
+     <?php include 'app/footer.php';?>
